2e5c7b5a6c19c49e8ff61d0cda5cdbf5c36864df
white9812 <white9812@gmail.com>
2024-04-08 00:07:40 +0900

    3rd q

--- week4.py
+++ week4.py
@@ -1,12 +1,66 @@
-n,m=map(int,input().split())
-squares=[list(map(int,input()))for _ in range(n)]
-quadrate=[]
-for k in range(n):
-    for i in range(n):
-        for j in range(m):
-            for h in range(m):
-               
-                        if squares[i][j]==squares[i][h]==squares[k][j]==squares[k][h] and abs(i-k) ==abs(h-j):
-                            quadrate.append(h-j)
-                            
-print(max(quadrate)+1)
+n=int(input())
+sentence=[input() for i in range(n)]
+# # print(sentence)
+
+def slump(s):
+        if len(s)==0:
+            return s
+        else:
+            i=1
+            while s[-i]=="F":
+                 i+=1
+                #  print(i)
+            # print(i)
+            if s[-i]=="D" or s[-i]=="E":
+                #  print(s)
+                 return slump(s[:-i])+s[-i:]
+
+def real_slump(s):
+     if s[-1]=="G" and slump(s[:-1])==s[:-1]:
+          return s         
+    
+        
+
+def Slimp(k):
+    if len(k)==2:
+        if k=="AH":
+            return k
+    elif len(k)>2:
+         if k[0]=="A" and k[-1]=="!" and k[1]=="B":  
+              return k[0]+k[1]+Slimp(k[2:-1])+k[-1:]
+         elif k[0]=="A" and k[-1]=="!":
+              if k[1:-1]==real_slump(k[1:-1]):
+                   return k[0]+real_slump(k[1:-1])+k[-1:]
+    
+              
+def slurpy(s):
+    if s[-1]=="!":
+         i=0
+         while s[i]!="A":
+              i+=1
+              return real_slump(s[:i+1]),Slimp(s[i+1,]),"NO"
+    elif s[-1]=="H" and s[-2]=="A":
+        return real_slump(s[:-2]),Slimp(s[-2:]),"NO"
+    elif s[-1]=="G":
+         i=1
+         while s[-i]!="!":
+              i+=1
+              return Slimp(s[:-i-1]),real_slump(s[-i-1:]),"YES"
+   
+         
+
+print("SLURPYS OUTPUT")
+for i in sentence:
+     print(slurpy(i)[2])
+print("END OF OUTPUT")
+  
+     
+     
+          
+     
+
+        
+
+                  
+                        
+                
